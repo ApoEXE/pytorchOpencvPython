1a71da5856d9bdf95f805789efb30173714316c9
Javier Pernia <javierpernia89@gmail.com>
2020-05-31 19:54:52 -0500

    combined all into one file canny yolo dnn screen conf cpp cuda opencv

--- opencv/cpp/opencv_dnn_cuda_yolov3/yolov3_opencv_dnn_cuda.cpp
+++ opencv/cpp/opencv_dnn_cuda_yolov3/yolov3_opencv_dnn_cuda.cpp
@@ -1,6 +1,6 @@
 // For more information and tips to improve inference FPS, visit https://github.com/opencv/opencv/pull/14827#issuecomment-568156546
-//g++ -g yolov3_opencv_dnn_cuda.cpp -o test `pkg-config opencv --cflags --libs`
-#include <iostream>
+//g++ yolov3_opencv_dnn_cuda.cpp -o test -pthread -lncurses `pkg-config opencv --cflags --libs`
+
 #include <queue>
 #include <iterator>
 #include <sstream>
@@ -14,20 +14,41 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/core/cuda.hpp>
 #include <opencv2/opencv.hpp>
+
 #ifdef _WIN32
 #include <io.h>
 #define access _access_s
 #else
 #include <unistd.h>
 #endif
+
+#include <iostream>
+#include <unistd.h>
+#include "conio.h"
+
+#define top 119        //w
+#define bottom 115     //s
+#define left 97        //a
+#define right 100      //d
+#define scale_up 120   //x
+#define scale_down 122 //z
+#define esc 27         //esc
+#define ratio 0.2      // w and h ratio
+#define minSizePorce 0.1
+#define l 108 // l minus lower
+#define k 107 // l + lower
+#define g 103 // h + higer
+#define h 104 // h minus higer
+int x = 0, y = 0;
+uint16_t black_h = 0, black_w = 0;
+uint16_t frame_h = 0, frame_w = 0;
+uint16_t new_frame_h = 0, new_frame_w = 0;
+//CANNY TRHESHOLDS
+uint8_t lowerThr = 30;
+uint8_t higherThr = lowerThr * 3;
+
 std::string absPath_weights = "/home/jav/wsl/weights/";
 std::string absPath_img = "/home/jav/wsl/images_videos/";
-bool FileExists(const std::string &Filename)
-{
-    bool check = access(Filename.c_str(), 0) == 0;
-    return check;
-}
-
 constexpr float confidence_threshold = 0.5;
 constexpr float nms_threshold = 0.4;
 constexpr int num_classes = 80;
@@ -40,29 +61,151 @@ const cv::Scalar colors[] = {
     {255, 0, 0}};
 const auto num_colors = sizeof(colors) / sizeof(colors[0]);
 
-int main()
+bool FileExists(const std::string &Filename)
+{
+    bool check = access(Filename.c_str(), 0) == 0;
+    return check;
+}
+
+void *getKey(void *t_id)
+{
+    while (true)
+    {
+        if (kbhit() != 0)
+        {
+            int key = getch();
+            //std::cout << key << std::endl;
+            switch (key)
+            {
+            case top:
+                std::cout << "TOP" << std::endl;
+                if (y > 0)
+                    y--;
+                break;
+            case bottom:
+                std::cout << "BOTTOM" << std::endl;
+                if ((y + new_frame_h) < black_h)
+                    y++;
+                break;
+            case left:
+                std::cout << "LEFT" << std::endl;
+                if (x > 0)
+                    x--;
+                break;
+            case right:
+                std::cout << "RIGHT" << std::endl;
+                if ((x + new_frame_w) < black_w)
+                    x++;
+                break;
+            case scale_up:
+
+                if (new_frame_w < black_w && ((new_frame_w + x) + (new_frame_w * ratio)) < black_w && new_frame_h < black_h && ((new_frame_h + y) + (new_frame_h * ratio)) < black_h)
+                {
+                    std::cout << "scale up" << std::endl;
+                    new_frame_w += new_frame_w * ratio;
+                    new_frame_h += new_frame_h * ratio;
+                }
+
+                break;
+            case scale_down:
+
+                if (new_frame_w > (minSizePorce * frame_w))
+                {
+                    std::cout << "scale down" << std::endl;
+                    new_frame_w -= new_frame_w * ratio;
+                    new_frame_h -= new_frame_h * ratio;
+                }
+                break;
+            case l:
+
+                if (lowerThr > 0)
+                {
+                    std::cout << "Low threshold Canny" << std::endl;
+                    lowerThr -= 1;
+                }
+
+                break;
+            case k:
+
+                if (lowerThr < 255)
+                {
+                    std::cout << "Low threshold Canny" << std::endl;
+                    lowerThr += 1;
+                }
+                break;
+            case h:
+
+                if (higherThr > 0)
+                {
+                    std::cout << "Higher threshold Canny" << std::endl;
+                    higherThr -= 1;
+                }
+
+                break;
+            case g:
+
+                if (higherThr < 999)
+                {
+                    std::cout << "Higher threshold Canny" << std::endl;
+                    higherThr += 1;
+                }
+                break;
+            case esc:
+                exit(0);
+                break;
+
+            default:
+                std::cout << "KEY NOT MAPPED" << std::endl;
+                break;
+            }
+        }
+    }
+}
+int main(int argc, char *argv[])
 {
     bool _default = true;
-    std::string classes = absPath_weights+"default/coco.names";
-    std::string weights = absPath_weights+"default/yolov3.weights";
-    std::string conf = absPath_weights+"default/yolov3.cfg";
+    std::string classes = absPath_weights + "default/coco.names";
+    std::string weights = absPath_weights + "default/yolov3.weights";
+    std::string conf = absPath_weights + "default/yolov3.cfg";
     //std::string videoPath = absPath_img+"busystreet.mp4";
     //std::string videoPath = absPath_img+"thermalDriving.mp4";
-    std::string videoPath = absPath_img+"prueba.mp4";
+    std::string videoPath = absPath_img + "prueba.mp4";
+    std::string lookPath = absPath_img + "blackest.jpg";
+    cv::Mat black = cv::imread(lookPath);
+    cv::Mat black_or = cv::imread(lookPath);
+    black_h = black.rows;
+    black_w = black.cols;
+
+    cv::cuda::GpuMat frameGpu;
+    cv::cuda::GpuMat resizeGpu;
+    cv::cuda::GpuMat blackGpu;
+    cv::cuda::GpuMat black_orGpu;
+    blackGpu.upload(black);
+    black_orGpu.upload(black_or);
+    //Thread
+    uint8_t id = 1;
+    int rc = 0;
+    pthread_t t;
+    rc = pthread_create(&t, NULL, getKey, (void *)id);
+    if (rc)
+    {
+        std::cout << "Error:unable to create thread," << rc << std::endl;
+        exit(-1);
+    }
     if (_default)
     {
-        classes = absPath_weights+"default/coco.names";
-        weights = absPath_weights+"default/yolov3.weights";
-        conf = absPath_weights+"default/yolov3.cfg";
+        classes = absPath_weights + "default/coco.names";
+        weights = absPath_weights + "default/yolov3.weights";
+        conf = absPath_weights + "default/yolov3.cfg";
     }
     else
     {
-        conf = absPath_weights+"custom/yolov3-obj.cfg";
+        conf = absPath_weights + "custom/yolov3-obj.cfg";
 
-        weights = absPath_weights+"custom//yolov3-obj_last.weights";
+        weights = absPath_weights + "custom//yolov3-obj_last.weights";
         //weights = "custom/thermal8.weights";
 
-        classes = absPath_weights+"custom/obj.names";
+        classes = absPath_weights + "custom/obj.names";
     }
 
     cv::cuda::GpuMat frameCuda;
@@ -85,9 +228,27 @@ int main()
         std::cerr << "check which one is missing: " << weights << " " << conf << " " << videoPath << "\n";
         return 0;
     }
-    //cv::VideoCapture source(0);
-    cv::VideoCapture source(videoPath);
 
+    //VIDEO CAPTURE
+    cv::VideoCapture source;
+    if (argc >= 2)
+    {
+        printf("Thermal");
+        source.open(2,cv::CAP_V4L);
+    }
+    else
+    {
+        source.open(0,cv::CAP_V4L);
+    }
+    
+
+    //cv::VideoCapture source(videoPath);
+    frame_h = source.get(cv::CAP_PROP_FRAME_HEIGHT);
+    frame_w = source.get(cv::CAP_PROP_FRAME_WIDTH);
+    new_frame_h = frame_h;
+    new_frame_w = frame_w;
+
+    //LOAD NET MODEL
     auto net = cv::dnn::readNetFromDarknet(conf, weights);
 
     net.setPreferableBackend(cv::dnn::DNN_BACKEND_CUDA);
@@ -98,7 +259,7 @@ int main()
     cv::Mat frame, blob;
     std::vector<cv::Mat> detections;
 
-    while (cv::waitKey(1) < 1)
+    while (source.isOpened() > 0)
     {
         source >> frame;
         if (frame.empty())
@@ -183,11 +344,21 @@ int main()
         frameCuda.upload(frame); //paso de CPU a GPU
         //GPU
         cv::cuda::cvtColor(frameCuda, gray_gpu, cv::COLOR_BGR2GRAY);
-        cv::Ptr<cv::cuda::CannyEdgeDetector> canny_edg = cv::cuda::createCannyEdgeDetector(20.0, 200.0, 5, true);
+
+        cv::Ptr<cv::cuda::CannyEdgeDetector> canny_edg = cv::cuda::createCannyEdgeDetector(lowerThr, higherThr, 3, true);
         canny_edg->detect(gray_gpu, imageCanny_gpu);
         frameCuda.setTo(cv::Scalar(0, 255, 0), imageCanny_gpu);
 
-        frameCuda.download(downFrame);
+        //DO CONFIG OF SCREEN BEFORE SHOWING
+
+        frameCuda.copyTo(resizeGpu);
+        //std::cout << "CONFIG----------------------" << new_frame_w << " " << new_frame_h;
+        cv::cuda::resize(resizeGpu, resizeGpu, cv::Size(new_frame_w, new_frame_h));
+        black_orGpu.copyTo(blackGpu);
+        resizeGpu.copyTo(blackGpu(cv::Rect(x, y, resizeGpu.cols, resizeGpu.rows)));
+        blackGpu.download(downFrame);
+
+        //frameCuda.download(downFrame);
         auto finish = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double> elapsed_time = finish - start;
         std::cout << "GPU CUDA Elapsed Time: " << elapsed_time.count() * 1000 << " msecs"
@@ -197,6 +368,7 @@ int main()
 
         cv::namedWindow("output");
         cv::imshow("output", frame);
+        cv::waitKey(1);
     }
 
     return 0;
